e254a19905fd509a614b56d2b40fcf86b4d5b5d4
JserWang <jserwang@gmail.com>
2021-08-21 17:15:35 +0800

    chore: types

--- packages/einfalt/src/node/index.ts
+++ packages/einfalt/src/node/index.ts
@@ -1,33 +1,18 @@
 export * from './config'
 export { createServer } from './server'
-// export { build } from './build'
-// export { optimizeDeps } from './optimizer'
-// export { send } from './server/send'
 export { createLogger } from './logger'
-// export { resolvePackageData, resolvePackageEntry } from './plugins/resolve'
 export { normalizePath } from './utils'
 
 // additional types
 export type {
   EinfaltDevServer,
   ServerOptions
-  // CorsOptions,
-  // FileSystemServeOptions,
-  // CorsOrigin,
-  // ServerHook,
-  // ResolvedServerOptions
 } from './server'
 export type {
   BuildOptions,
-  // LibraryOptions,
-  // LibraryFormats,
   ResolvedBuildOptions
 } from './build'
-// export type {
-//   DepOptimizationMetadata,
-//   DepOptimizationOptions
-// } from './optimizer'
-// export type { Plugin } from './plugin'
+
 export type {
   Logger,
   LogOptions,
@@ -43,3 +28,6 @@ export type {
   ComponentsOptions,
   ComponentResolver
 } from './plugins/components'
+export type {
+  SpacingOptions
+} from './plugins/spacing'
